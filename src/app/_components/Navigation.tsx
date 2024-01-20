bcf9f64795bd1fb3c0a716868f9dded02dc389fc
daku10 <daku10.dev@gmail.com>
2024-01-20 14:21:26 +0900

    remove unused files

--- src/app/_components/Navigation.tsx
+++ src/app/_components/Navigation.tsx
@@ -12,20 +12,6 @@ export const Navigation = () => {
       <ul className="flex">
         <li>
           <Link
-            href="/blog"
-            className={cn(
-              "text-secondary hover:text-primary px-4 text-lg hover:underline hover:underline-offset-8",
-              {
-                "text-primary underline underline-offset-8":
-                  pathname.startsWith("/blog") || pathname.startsWith("/posts"),
-              },
-            )}
-          >
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link
             href="/tags"
             className={cn(
               "text-secondary hover:text-primary px-4 text-lg hover:underline hover:underline-offset-8",
