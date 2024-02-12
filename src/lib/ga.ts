c65f113f6bc743f9f1fcf4e02e73126de24698ad
daku10 <daku10.dev@gmail.com>
2024-02-12 12:42:23 +0900

    replace shikiji to shiki and remove unused packages

--- src/lib/ga.ts
+++ src/lib/ga.ts
@@ -2,9 +2,3 @@ import { env } from "@/env.mjs";
 
 export const GA_ID = env.NEXT_PUBLIC_GA_ID;
 export const isEnabled = env.NEXT_PUBLIC_APP_ENV === "production";
-export const pageview = (url: string) => {
-  if (!isEnabled) return;
-  window.gtag("config", GA_ID, {
-    page_path: url,
-  });
-};
