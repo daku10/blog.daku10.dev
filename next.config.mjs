699c5a4810a7ab21f251e891bd629b26dc482185
daku10 <daku10.dev@gmail.com>
2024-04-18 21:14:08 +0900

    add @next/bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 import "./src/env.mjs";
 
+const withBundleAnalyzer =
+  process.env.ANALYZE === "true"
+    ? async (config) => {
+        const { default: analyzer } = await import("@next/bundle-analyzer");
+        return analyzer({
+          enabled: true,
+        })(config);
+      }
+    : (config) => config;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -8,4 +18,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
